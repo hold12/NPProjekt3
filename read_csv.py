badf034082d593494458130c66cfa148748a9b1b
Anders W. Olsen <anders@wiberg.tech>
2017-04-26 18:48:24 +0200

    added script to read csv and calculate

---
+++ read_csv.py
@@ -0,0 +1,19 @@
+import csv
+with open('data.csv', 'rb') as f:
+   reader = csv.reader(f)
+   csv_list = list(reader)
+
+highest_value = 0
+
+for i in csv_list[1::]:
+   itotal = 0
+   for j in i[1::]:
+        itotal += int(j)
+
+   if itotal > highest_value:
+      highest_value = itotal
+
+file = open('highest.txt', 'w')
+file.write(str(highest_value))
+file.close
+print highest_value
